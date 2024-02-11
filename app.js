1f37c9173e7aea1bb3262059eccf713cb7ee1202
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-12 00:02:51 +0530

    viewmypost

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const cors=require("cors")
 const mongoose=require("mongoose")
 const blogRoutes=require("./controllers/blogRouter")
 const postRoutes=require("./controllers/postRouter")
+const ownRoutes=require("./controllers/ownPostRouter")
 
 const app=express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/api/blog",blogRoutes)
-app.use("/api/post/",postRoutes)
+app.use("/api/post",postRoutes)
+app.use("/api/ownpost",ownRoutes)
 
 mongoose.connect("mongodb+srv://arun:arun123@cluster0.5bjnd.mongodb.net/blogDb?retryWrites=true&w=majority",
     { useNewUrlParser: true })
